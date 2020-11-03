85f65e867193a28eafe1c8d086e28fd98d1c5663
Diego Prestes <diegoprestes.git@hotmail.com>
2020-11-02 23:24:10 -0300

    feat: README feito, padrao de projeto aplicado

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// const express = require('express')
+// const mongoose = require('mongoose')
+
+// const app = express()
+
+// mongoose.connect('mongodp://localhost:27107/nodeapi', { useNewUrlParser: true })
+
+// app.get('/', (req, res) => {
+//   res.send('GOOGLE')
+// })
+
+// app.listen(3001, () => {
+//   console.log('running on 3001')
+// })
